ccfdb50d865bffe88058504a16492c6ecc6973f1
cat13 <37980174+cat3306@users.noreply.github.com>
2025-11-21 15:45:10 +0800

    up

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -14,7 +14,7 @@ import (
 )
 
 // NewGRPCServer new a gRPC server.
-func NewGRPCServer(c *conf.Server, thirdModule *thirdmodule.Module, greeter *service.Service, logger log.Logger) *grpc.Server {
+func NewGRPCServer(c *conf.Server, thirdModule *thirdmodule.Module, service *service.Service, logger log.Logger) *grpc.Server {
 	var opts = []grpc.ServerOption{
 		grpc.Middleware(
 			recovery.Recovery(),
@@ -34,6 +34,6 @@ func NewGRPCServer(c *conf.Server, thirdModule *thirdmodule.Module, greeter *ser
 		opts = append(opts, grpc.Timeout(c.Grpc.Timeout.AsDuration()))
 	}
 	srv := grpc.NewServer(opts...)
-	v1.RegisterServerServer(srv, greeter)
+	v1.RegisterServerServer(srv, service)
 	return srv
 }
--- internal/server/http.go
+++ internal/server/http.go
@@ -14,7 +14,7 @@ import (
 )
 
 // NewHTTPServer new an HTTP server.
-func NewHTTPServer(c *conf.Server, thirdModule *thirdmodule.Module, greeter *service.Service, logger log.Logger) *http.Server {
+func NewHTTPServer(c *conf.Server, thirdModule *thirdmodule.Module, service *service.Service, logger log.Logger) *http.Server {
 	var opts = []http.ServerOption{
 		http.Middleware(
 			recovery.Recovery(),
@@ -34,6 +34,6 @@ func NewHTTPServer(c *conf.Server, thirdModule *thirdmodule.Module, greeter *ser
 		opts = append(opts, http.Timeout(c.Http.Timeout.AsDuration()))
 	}
 	srv := http.NewServer(opts...)
-	v1.RegisterServerHTTPServer(srv, greeter)
+	v1.RegisterServerHTTPServer(srv, service)
 	return srv
 }
